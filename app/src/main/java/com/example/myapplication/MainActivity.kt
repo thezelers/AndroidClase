31afb7ccbdb6d456d2301f8a7fbec9a95e7d0b23
Carlos <carlos.rey@live.u-tad.com>
2019-10-04 11:27:08 +0200

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,25 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val helloText: TextView = findViewById(R.id.holaworld)
-        val btnchange: Button = findViewById(R.id.btnChange)
-        val numerotxt : TextView = findViewById(R.id.txtNumero)
+            helloText.text = R.string.frase1.toString()
 
-        var numero : Int = 1
-        var aux : Int = 0
-        numerotxt.text = numero.toString()
-
-
-
-        val alerta = getString(R.string.btnChange)
-        btnchange.setOnClickListener {
-            Toast.makeText(MainActivity@this, alerta, Toast.LENGTH_SHORT).show()
-            numero++
-            numerotxt.text = numero.toString()
-        }
+    }
     }
 
 
 
 
 
-}
+
