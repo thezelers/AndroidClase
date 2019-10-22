47b9eeed13ccb3d8385d7ea639b64979b64e4e9b
Carlos <carlos.rey@live.u-tad.com>
2019-10-22 19:04:52 +0200

    Ejercicio 3

--- app/src/main/java/com/example/myapplication/MovieAdapter.kt
+++ app/src/main/java/com/example/myapplication/MovieAdapter.kt
@@ -2,27 +2,52 @@ package com.example.myapplication
 
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 
-class MovieAdapter(val stringList: List<String>): RecyclerView.Adapter<StringListViewHolder>() {
+class MovieAdapter(val imageList: List<String>,val titleList: List<String>,val rDateList: List<String>,val rateList: List<String>,val titleOList: List<String>): RecyclerView.Adapter<movieListViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringListViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.string_item,parent,false)
-        return StringListViewHolder(view as TextView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): movieListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item,parent,false)
+        return movieListViewHolder(view)
     }
 
-    override fun getItemCount(): Int = stringList.size
+    override fun getItemCount(): Int = titleList.size
 
-    override fun onBindViewHolder(holder: StringListViewHolder, position: Int) {
-       val stringItem = stringList[position]
-        holder.textview.text = stringItem
+    override fun onBindViewHolder(holder: movieListViewHolder, position: Int) {
+        val titleItem = titleList[position]
+        val titleOItem = titleOList[position]
+        val rDateItem = rDateList[position]
+        val rateItem = rateList[position]
+        val imageItem = imageList[position]
+        holder.bind(rateItem,rDateItem,titleOItem,titleItem,imageItem)
+
 
     }
 }
 
-class StringListViewHolder(val textview: TextView): RecyclerView.ViewHolder(textview){
+class movieListViewHolder(val myview: View): RecyclerView.ViewHolder(myview){
+    private val movieImage:ImageView =  myview.findViewById(R.id.movieImage)
+    private val movieTitle:TextView =  myview.findViewById(R.id.movieTitleTxt)
+    private val movieTitleO:TextView =  myview.findViewById(R.id.movieTitleOTxt)
+    private val movieRateTxt:TextView =  myview.findViewById(R.id.movieRateTxt)
+    private val movieRDateTxt:TextView =  myview.findViewById(R.id.movieRDateTxt)
+    fun bind(rate:String,rDate:String,titleO:String,title:String,image:String) {
+        Picasso.get().load(image).into(movieImage)
+        movieTitle.setText(title)
+        movieTitleO.setText(titleO)
+        movieRateTxt.setText(rate)
+        movieRDateTxt.setText(rDate)
+    }
+
+
+
+
 
 }
--- app/src/main/java/com/example/myapplication/MovieListActivity.kt
+++ app/src/main/java/com/example/myapplication/MovieListActivity.kt
@@ -7,16 +7,21 @@ import kotlinx.android.synthetic.main.activity_movie_list.*
 
 class MovieListActivity : AppCompatActivity() {
 
-    private val stringlist = (1..10).map{
-        "Element nº $it"
-    }
+    private val imageList: List<String> = mutableListOf("https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/3/39/Terminator_3_Rise_of_the_Machines_movie.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/9/95/Terminator-salvation-poster.jpg")
+    private val titleList: List<String> = mutableListOf("Terminator","Terminator 2: el juicio final","Terminator 3: La rebelión de las máquinas","Terminator Salvation")
+    private val rDateList: List<String> = mutableListOf("1984","1991","2003","2009")
+    private val rateList: List<String> = mutableListOf("97","93","68","33")
+    private val titleOList: List<String> = mutableListOf("Terminator","Terminator 2: Judgment Day","Terminator 3: Rise of the Machines","Terminator Salvation")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
 
         movie_list.layoutManager = LinearLayoutManager(this)
-        movie_list.adapter = MovieAdapter(stringlist)
+        movie_list.adapter = MovieAdapter(imageList,titleList,rDateList,rateList,titleOList)
     }
 
 
